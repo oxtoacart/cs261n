0502f2e7fd9cea1bff7e6c71122e8ac018e23c01
David Fifield <david@bamsoftware.com>
2014-07-29 13:59:33 -0700

    binwidth=5 in diagrams.

--- diagrams.R
+++ diagrams.R
@@ -11,20 +11,20 @@ x <- x[order(x$t),]
 lbl = x[x$source=="lbl",]
 
 png("diagrams/datalen.png")
-qplot(lbl$datalen, geom="histogram", main="TCP payload length", binwidth=2) + xlab("length")  
+qplot(lbl$datalen, geom="histogram", main="TCP payload length", binwidth=5) + xlab("length")  
 
 dev.off()
 
 png("diagrams/datalen-in.png")
-qplot(lbl[lbl$dir=="IN",]$datalen, main="TCP payload length (incoming)", binwidth=2) + xlab("length") 
+qplot(lbl[lbl$dir=="IN",]$datalen, main="TCP payload length (incoming)", binwidth=5) + xlab("length") 
 dev.off()
 
 png("diagrams/datalen-out.png")
-qplot(lbl[lbl$dir=="OUT",]$datalen, main="TCP payload length (outgoing)", binwidth=2) + xlab("length") 
+qplot(lbl[lbl$dir=="OUT",]$datalen, main="TCP payload length (outgoing)", binwidth=5) + xlab("length") 
 dev.off()
 
 png("diagrams/datalen-google.png")
-qplot(lbl[lbl$google,]$datalen, main="TCP payload length (Google only)", binwidth=2) + xlab("length") 
+qplot(lbl[lbl$google,]$datalen, main="TCP payload length (Google only)", binwidth=5) + xlab("length") 
 dev.off()
 
 png("diagrams/datalen-google-cdf.png")
@@ -32,15 +32,15 @@ qplot(lbl[lbl$google,]$datalen, stat="ecdf", geom="step", xlim=range(lbl$datalen
 dev.off()
 
 png("diagrams/datalen-google-in.png")
-qplot(lbl[lbl$google & lbl$dir=="IN",]$datalen, main="TCP payload length (incoming, Google only)", binwidth=2) + xlab("length") 
+qplot(lbl[lbl$google & lbl$dir=="IN",]$datalen, main="TCP payload length (incoming, Google only)", binwidth=5) + xlab("length") 
 dev.off()
 
 png("diagrams/datalen-google-out.png")
-qplot(lbl[lbl$google & lbl$dir=="OUT",]$datalen, main="TCP payload length (outgoing, Google only)", binwidth=2) + xlab("length") 
+qplot(lbl[lbl$google & lbl$dir=="OUT",]$datalen, main="TCP payload length (outgoing, Google only)", binwidth=5) + xlab("length") 
 dev.off()
 
 png("diagrams/datalen-tbb.png")
-qplot(x[x$source=="tbb",]$datalen, main="TCP payload length (TBB meek)", binwidth=2) + xlab("Length") 
+qplot(x[x$source=="tbb",]$datalen, main="TCP payload length (TBB meek)", binwidth=5) + xlab("Length") 
 dev.off()
 
 png("diagrams/datalen-tbb-cdf.png")
@@ -54,7 +54,7 @@ intervals <- Reduce(append, lapply(levels(syn.out$src), function(src) {
 	diff(syn.out[syn.out$src==src,]$t)
 }))
 postscript("diagrams/syninterval-out.eps")
-qplot(intervals, geom="histogram", main="Interval between SYNs", binwidth=2) + xlab("time elapsed between SYNs") 
+qplot(intervals, geom="histogram", main="Interval between SYNs", binwidth=5) + xlab("time elapsed between SYNs") 
 dev.off()
 postscript("diagrams/syninterval-out-log.eps")
 qplot(intervals, geom="histogram", main="Interval between SYNs", binwidth=0.02) + xlab("time elapsed between SYNs")+ scale_x_log10(breaks=c(0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000)) 
@@ -65,7 +65,7 @@ intervals <- Reduce(append, lapply(levels(syn.google.out$src), function(src) {
 	diff(syn.google.out[syn.google.out$src==src,]$t)
 }))
 postscript("diagrams/syninterval-google-out.eps")
-qplot(intervals, geom="histogram", main="Interval between SYNs (Google only)", binwidth=2) + xlab("time elapsed between SYNs") 
+qplot(intervals, geom="histogram", main="Interval between SYNs (Google only)", binwidth=5) + xlab("time elapsed between SYNs") 
 dev.off()
 postscript("diagrams/syninterval-google-out-log.eps")
 qplot(intervals, geom="histogram", main="Interval between SYNs (Google only)", binwidth=0.02) + xlab("time elapsed between SYNs")+ scale_x_log10(breaks=c(0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000)) 
