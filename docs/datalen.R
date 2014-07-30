28c7f8b0e9303f88e96684edccc49ab37002be96
David Fifield <fifield@eecs.berkeley.edu>
2014-07-30 15:54:01 -0700

    Use facet_wrap for datalen.R.

--- docs/datalen.R
+++ docs/datalen.R
@@ -2,19 +2,6 @@ library(ggplot2)
 
 col.names <- c("t", "src", "sport", "dst", "dport", "dir", "iplen", "datalen", "syn")
 
-# http://www.cookbook-r.com/Graphs/Facets_%28ggplot2%29/#modifying-facet-label-text
-label = function(variable, value) {
-	value <- as.character(value)
-	if (variable == "source") {
-		value[value == "lbl"] <- "LBL Google HTTPS"
-		value[value == "tbb"] <- "meek on App Engine"
-	} else if (variable == "dir") {
-		value[value == "IN"] <- "inbound"
-		value[value == "OUT"] <- "outbound"
-	}
-	return(value)
-}
-
 x <- cbind(read.table("../traces/lbl.https.goog.dpriv.tcp.log", col.names=col.names), google=T, source="lbl")
 x <- rbind(x, cbind(read.table("../traces/lbl.https.non-goog.dpriv.tcp.log", col.names=col.names), google=F, source="lbl"))
 x <- rbind(x, cbind(subset(read.table("../traces/meek_tbb_extension_tcp.pcap.tcp.log", col.names=col.names), dport==443), google=T, source="tbb"))
@@ -36,9 +23,11 @@ t <- table(x[x$google & x$source=="tbb", ]$datalen)
 
 binwidth = 5
 
+x <- cbind(x, label=ifelse(x$source=="lbl", "LBL Google HTTPS", "meek on App Engine"))
+
 p <- ggplot(x[x$google, ], aes(datalen))
 p <- p + geom_histogram(aes(y=..density..), binwidth=binwidth)
 p <- p + xlab("TCP payload length") + ylab("Fraction of TCP segments")
 p <- p + scale_y_continuous(labels = function(y) binwidth * y)
-p <- p + facet_grid(source ~ ., labeller = label)
+p <- p + facet_wrap(~ label, ncol=1, scales="fixed")
 ggsave("datalen.pdf", width=4, height=3.75, units="in")
